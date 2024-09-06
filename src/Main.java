61dae8f410620d7c42cc0883202fd316394d4953
barto3214 <bartek.slusarz@onet.pl>
2024-09-06 10:26:25 +0200

    Wczytywanie liczby z klawiatury

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        int wylosowanaliczba = (int) (Math.random()*100+1);
+        System.out.println(wylosowanaliczba);
+
+
+        //wczytywanie z klawiatury
+        Scanner klawiatura = new Scanner(System.in);
+        System.out.println("Podaj liczbę\n");
+        int wpisanaliczba = klawiatura.nextInt();
+
     }
 }
