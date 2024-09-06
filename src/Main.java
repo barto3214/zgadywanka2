39973dfb22dedbc86b7b2af1026722b24862d3bb
barto3214 <bartek.slusarz@onet.pl>
2024-09-06 10:34:31 +0200

    wyrażenie war

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,17 @@ public class Main {
         System.out.println("Podaj liczbę\n");
         int wpisanaliczba = klawiatura.nextInt();
 
+        // instrukcja warunkowa
+        if(wpisanaliczba == wylosowanaliczba){
+            System.out.println("Wygrałeś");
+        } else if (wpisanaliczba != wylosowanaliczba) {
+            System.out.println("Nie trafiłeś");
+        }
+        //określić jak duża jest różnica między wyborem a wylosowanym
+        //wyrażenie warunkowe != instrukcja warunkowa bo wyrażenie zwraca wartośc
+        int roznica = wpisanaliczba>wylosowanaliczba ? //takie {} w ifie
+                wpisanaliczba - wylosowanaliczba: // else if
+                wylosowanaliczba-wpisanaliczba;
+        System.out.println("Pomyłka o "+ roznica);
     }
 }
